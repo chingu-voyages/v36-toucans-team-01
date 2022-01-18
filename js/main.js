d8d022a79f08aeba33857a9947a5419da408322a
i-f-r-a-h <ifrahvermeer2@gmail.com>
2022-01-18 15:37:48 +0000

    feat: style review indicators

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
+const myCarousel = document.querySelector('#myCarousel')
+const carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: false
 })
